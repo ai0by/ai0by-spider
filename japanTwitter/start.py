64cc1c2f74ca04753f3966dc73ed013dc7adb022
Administrator <killnetsec@gmail.com>
2019-03-21 10:17:42 +0800

    岛国推特妹子爬虫

---
+++ japanTwitter/start.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import requests
+import urllib2
+import random
+
+def spy(url):
+    req = urllib2.Request(url)
+    req = urllib2.urlopen(req)
+    page = req.read()
+    soup = BeautifulSoup(page, "html.parser")
+    for imgSoup in soup.find_all('div', {"class": "row"}):
+        for i in imgSoup.find_all('div', {'class': 'photo'}):
+            for j in i.find('div', {'class': 'photo-link-outer'}).find('a').find_all('img'):
+                img = j.get("src")
+                print img
+                str = random.sample('zyxwvutsrqponmlkjihgfedcba', 6)
+                downImg(img, str)
+    nexturl = soup.find('p',{'class':'go-to-next-page'})
+    nexturl = nexturl.find('a').get('href')
+    pageurl = "http://jigadori.fkoji.com"+nexturl
+    spy(pageurl)
+
+def downImg(img,m):
+    try:
+        r = requests.get(img)
+    except Exception , e:
+        print "图片获取失败"
+        return
+    with open('./img/good%s.jpg' % m, 'wb') as f:
+        f.write(r.content)
+if __name__ == '__main__':
+    url = "http://jigadori.fkoji.com"
+    spy(url)
